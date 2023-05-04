84d620f91ad021fac742ebfba8115680b1b47871
Blaine Hamilton <blaineh7@marriottschool.byu.edu>
2023-05-04 10:27:25 -0600

    Update helloteam.js

--- helloteam.js
+++ helloteam.js
@@ -1,1 +1,7 @@
-const names = ["Caroline", "Tyler" , "Christopher","Jose", "Ricky"]
+const names = ["Caroline", "Tyler" , "Christopher","Jose", "Ricky", "Brother Hamilton"]
+
+
+names.forEach((item, index, array) => {
+    console.log(array[index] + " is table 1 member " + index);
+}
+);
