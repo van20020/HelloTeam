5d0017feaa1c5ad0cfe04075c68be00ec9f153c4
Blaine Hamilton <blaineh7@marriottschool.byu.edu>
2023-05-04 10:45:04 -0600

    array change

--- helloteam.js
+++ helloteam.js
@@ -2,6 +2,6 @@ const names = ["Caroline", "Tyler" , "Christopher","Jose", "Ricky", "Brother Ham
 
 
 names.forEach((person, index, array) => {
-    console.log(person + " is table 1 member " + index + " who is before " + array[index + 1]);
+    console.log(person + " is table 1 member #" + index + " who is before team member #" + (index+1) + " named " + array[index + 1]);
 }
 );
